fd036dd54ff898942e77521164cb416b20456e02
Adam Rubin <adam@richminds.com>
2013-11-04 21:09:52 -0500

    Some extra relationships which may be of use later on

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -1,5 +1,7 @@
 class Practice < ActiveRecord::Base
   has_many :coaches
+  has_many :programs, through: :coaches
+  has_many :users, through: :programs
 
   accepts_nested_attributes_for :coaches
 end
