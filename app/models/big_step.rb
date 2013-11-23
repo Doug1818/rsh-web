c9ecf486c13cc5001754eb498b47b7eab9e8de7f
Adam Rubin <adam@richminds.com>
2013-11-22 23:45:00 -0500

    WIP

--- app/models/big_step.rb
+++ app/models/big_step.rb
@@ -1,4 +1,8 @@
 class BigStep < ActiveRecord::Base
   belongs_to :program
   has_many :small_steps
+
+  def to_s
+    "#{name}"
+  end
 end
--- app/models/small_step.rb
+++ app/models/small_step.rb
@@ -1,6 +1,7 @@
 class SmallStep < ActiveRecord::Base
   FREQUENCIES = { "Daily" => 0, "# Times Per Week" => 1, "Specific Days of the Week" => 2 }
 
+  belongs_to :program
   belongs_to :big_step
   has_many :small_step_activites, dependent: :destroy
 end
