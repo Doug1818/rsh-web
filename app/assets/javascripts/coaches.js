9dff97e47fae05ca705b4fc660a6ac8b723e232b
Josh Boehringer <josh@healthtalker.com>
2013-12-10 18:09:14 -0500

    Fixed Gender placeholder for coaches form, fixed alignment of primary btns.

--- app/assets/javascripts/coaches.js
+++ app/assets/javascripts/coaches.js
@@ -1,6 +1,11 @@
 jQuery(document).ready(function($) {
 
-  $("#coach_gender, #coach_status").select2({
+  $("#coach_gender").select2({
+    minimumResultsForSearch: -1,
+    placeholder: 'Gender'
+  });
+
+  $("#coach_status").select2({
     minimumResultsForSearch: -1
   });
 
