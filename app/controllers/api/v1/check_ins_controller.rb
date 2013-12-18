4ea62feff3037e7f011953d0b0919f5ea5546bf4
prouty <nate.prouty@infonate.com>
2013-12-18 12:46:37 -0500

    Check in no longer has a small_step_id

--- app/controllers/api/v1/check_ins_controller.rb
+++ app/controllers/api/v1/check_ins_controller.rb
@@ -6,17 +6,13 @@ class Api::V1::CheckInsController < Api::V1::ApplicationController
 
     status = params[:status]
 
-    @small_step = @program.small_steps.find small_step_id
+    @check_in = CheckIn.where(created_at: date).first_or_create(week_id: week_id)
+    @check_in.activities.create(status: status, created_at: date, small_step_id: small_step_id)
+    @check_in.save!
 
-    if @small_step.present?
-      @check_in = CheckIn.where(created_at: date, small_step_id: small_step_id).first_or_create(week_id: week_id)
-      @check_in.activities.create(status: status, created_at: date, small_step_id: small_step_id)
-      @check_in.save!
-
-      render status: 200, json: {
-        success: true,
-        data: {}
-      }
-    end
+    render status: 200, json: {
+      success: true,
+      data: {}
+    }
   end
 end
