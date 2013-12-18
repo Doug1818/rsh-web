70d973168d3ec06088083103f8fb79e3aaaf0184
prouty <nate.prouty@infonate.com>
2013-12-18 12:08:00 -0500

    When creating check in activity, add activity to a current check in if it exists, don't create a new check in.

--- app/controllers/api/v1/check_ins_controller.rb
+++ app/controllers/api/v1/check_ins_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::CheckInsController < Api::V1::ApplicationController
     @small_step = @program.small_steps.find small_step_id
 
     if @small_step.present?
-      @check_in = @small_step.check_ins.create(created_at: date, week_id: week_id)
+      @check_in = CheckIn.where(created_at: date, small_step_id: small_step_id).first_or_create(week_id: week_id)
       @check_in.activities.create(status: status, created_at: date, small_step_id: small_step_id)
       @check_in.save!
 
