374ee20fdbbdc1d744d059487df40264c32da412
prouty <nate.prouty@infonate.com>
2013-12-19 15:45:05 -0500

    Updated the check in API to do small step check ins as a group.

--- app/controllers/api/v1/check_ins_controller.rb
+++ app/controllers/api/v1/check_ins_controller.rb
@@ -1,14 +1,16 @@
 class Api::V1::CheckInsController < Api::V1::ApplicationController
   def create
+    status = params[:status]
     week_id = params[:week_id]
-    small_step_id = params[:small_step_id]
+    small_steps = params[:small_steps]
     date = Date.parse(params[:date])
 
-    status = params[:status]
+    @check_in = @program.check_ins.where(created_at: date, week_id: week_id).first_or_create
 
-    @check_in = CheckIn.where(created_at: date).first_or_create(week_id: week_id)
-    @check_in.activities.create(status: status, created_at: date, small_step_id: small_step_id)
-    @check_in.save!
+    small_steps.each do |small_step|
+      @check_in.activities.create(status: status, created_at: date, small_step_id: small_step[:id])
+      @check_in.save!
+    end
 
     render status: 200, json: {
       success: true,
--- app/controllers/api/v1/weeks_controller.rb
+++ app/controllers/api/v1/weeks_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::WeeksController < Api::V1::ApplicationController
   # Get the current week for the current program
   def index
     date = params[:date]
-    @week = @program.weeks.where("DATE(?) BETWEEN start_date AND end_date", date).as_json(include: :small_steps)
+    @week = @program.weeks.where("DATE(?) BETWEEN start_date AND end_date", date).as_json(include: { small_steps: { only: [:id, :name]}})
 
     render status: 200, json: {
       success: true,
