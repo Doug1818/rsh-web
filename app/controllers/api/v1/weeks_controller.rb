5f24b0ba59d8d75310f637886d5eaa1dd09c4504
prouty <nate.prouty@infonate.com>
2013-12-22 22:56:50 -0500

    Changed API week view to only grab days of the current week.

--- app/controllers/api/v1/weeks_controller.rb
+++ app/controllers/api/v1/weeks_controller.rb
@@ -2,30 +2,31 @@ class Api::V1::WeeksController < Api::V1::ApplicationController
   
   # Get all of the weeks for a program
   def index
-    @program = @program.decorate
-    @weeks = @program.weeks.includes(:check_ins).as_json(except: [:program_id, :created_at, :updated_at])
+    date = params[:date]
+    today = Date.today
 
-    @weeks.each do |week| 
+    @program = @program.decorate
+    @week = @program.weeks.where("DATE(?) BETWEEN start_date AND end_date", date).includes(:check_ins).as_json(except: [:program_id, :created_at, :updated_at])
+
+    @week.each do |week| 
       start_date = week['start_date']
       end_date = week['end_date']
 
       week[:days] = Array.new
 
-      @week = @program.weeks.find week['id']
+      @current_week = @program.weeks.find week['id']
 
       (start_date..end_date).each do |date|
-        check_in = @week.find_check_in_for_day(date)
+        check_in = @current_week.find_check_in_for_day(date)
         check_in_status = check_in.is_a?(CheckIn) ? check_in.status : 0
-        is_future = date > Date.today.to_time_in_current_zone # TODO: Use user's timezone
+        is_future = date.beginning_of_day.in_time_zone('EST') > today.beginning_of_day.in_time_zone('EST') # TODO: Use user's timezone
         week[:days] << {date: date.strftime('%b %e'), day_number: @program.day_number(date), check_in_status: check_in_status, is_future: is_future }
       end
     end
 
-    puts @weeks
-
     render status: 200, json: {
       success: true,
-      data: { weeks: @weeks }
+      data: { week: @week }
     }
   end
 
