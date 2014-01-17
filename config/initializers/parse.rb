47658ed3e44200cf76229058337b6bfc6d437b63
Adam Rubin <adam@richminds.com>
2014-01-17 15:40:33 -0500

    start to conditionalize parse config based on env

--- config/initializers/parse.rb
+++ config/initializers/parse.rb
@@ -1,1 +1,5 @@
-Parse.init application_id: "ZQbu9JMhoTRa42q6etZ3tf8slARx47rMJNkBPreg", api_key: "2Lfnx3dV6Vfrno2KF0geaZcebFaQj6cAI4ZTQDgW"
+if Rails.env.development?
+  Parse.init application_id: "ZQbu9JMhoTRa42q6etZ3tf8slARx47rMJNkBPreg", api_key: "2Lfnx3dV6Vfrno2KF0geaZcebFaQj6cAI4ZTQDgW"
+else
+  Parse.init application_id: "ZQbu9JMhoTRa42q6etZ3tf8slARx47rMJNkBPreg", api_key: "2Lfnx3dV6Vfrno2KF0geaZcebFaQj6cAI4ZTQDgW"
+end
