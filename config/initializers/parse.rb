0e67a78e8e543acf261982a9f5969a2a13499575
Doug Raicek <doug.raicek@gmail.com>
2014-01-17 21:27:26 -0500

    Add Parse api keys for development and production

--- config/initializers/parse.rb
+++ config/initializers/parse.rb
@@ -1,5 +1,5 @@
 if Rails.env.development?
-  Parse.init application_id: "ZQbu9JMhoTRa42q6etZ3tf8slARx47rMJNkBPreg", api_key: "2Lfnx3dV6Vfrno2KF0geaZcebFaQj6cAI4ZTQDgW"
+  Parse.init application_id: "0I8WCIxThhO07OAIeqKDFwAq0rHRXnHxY5auF6KG", api_key: "QRAhXyWISyBpJmckbiY49a1XtjwlCKhAj2PzMoFR"
 else
-  Parse.init application_id: "ZQbu9JMhoTRa42q6etZ3tf8slARx47rMJNkBPreg", api_key: "2Lfnx3dV6Vfrno2KF0geaZcebFaQj6cAI4ZTQDgW"
+  Parse.init application_id: "m7UOgUNoihu3JP3Zn9WcShFORaxkoanuTybOWDx8", api_key: "dpQ2WWwn2TXjN5pYbCL5laAZSjdd4dfn48LdFx68"
 end
