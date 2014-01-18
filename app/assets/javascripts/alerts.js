d4e5ffd81aaa6dd8c9d0d03655738c0ec8c6d104
Doug Raicek <doug.raicek@gmail.com>
2014-01-18 16:03:09 -0500

    Move alerts plus button below alert form

--- app/assets/javascripts/alerts.js
+++ app/assets/javascripts/alerts.js
@@ -1,8 +1,8 @@
 // show the form for adding a new alert
 jQuery(document).ready(function($) {
   $(document).on('click','.new-alert-link',function (event) {
-    console.log( $(this).next().attr("class") );
-    $(this).next().find(".form").show();
+    //console.log( $(this).prev().prev().attr("class") );
+    $(this).prev().prev().find(".form").show();
     event.preventDefault(); // Prevent link from following its href
   });
 });
