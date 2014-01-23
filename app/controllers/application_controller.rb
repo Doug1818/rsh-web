5c14d399c9ebaf2e8e30706ba9dad477c28ba5e4
Doug Raicek <doug.raicek@gmail.com>
2014-01-23 11:14:56 -0500

    Add around_filter for coach times zones to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :store_referral_code
+  around_filter :coach_time_zone, if: :current_coach
 
   def store_referral_code
     if params[:referral_code]
@@ -18,4 +19,9 @@ class ApplicationController < ActionController::Base
     current_coach.practice if current_coach
   end
   helper_method :current_practice
+
+  # Set coach timezone on a per request bas
+  def coach_time_zone(&block)
+    Time.use_zone(current_coach.timezone, &block)
+  end
 end
