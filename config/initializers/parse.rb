3ac0c4fd6aa79905184ecce85bb6a317e5489e19
Doug Raicek <doug.raicek@gmail.com>
2014-01-24 14:51:14 -0500

    Remove commented out code in parse.rb

--- config/initializers/parse.rb
+++ config/initializers/parse.rb
@@ -1,8 +1,5 @@
 if Rails.env.development?
-  Parse.init application_id: "0I8WCIxThhO07OAIeqKDFwAq0rHRXnHxY5auF6KG", api_key: "RaBSqZgrwrNeDvvFN6vX63c9Ha02WDkGuwuzd24V" # dev
-  # Parse.init application_id: "m7UOgUNoihu3JP3Zn9WcShFORaxkoanuTybOWDx8", api_key: "sHlZbb3wUgFoVXYPaCmRirwWx0LE2HNO3J4twNwK" # production
+  Parse.init application_id: "0I8WCIxThhO07OAIeqKDFwAq0rHRXnHxY5auF6KG", api_key: "RaBSqZgrwrNeDvvFN6vX63c9Ha02WDkGuwuzd24V"
 else
-	# Parse.init application_id: "0I8WCIxThhO07OAIeqKDFwAq0rHRXnHxY5auF6KG", api_key: "RaBSqZgrwrNeDvvFN6vX63c9Ha02WDkGuwuzd24V" # dev
-	# Figure out better solution to line up testflight + heroku with development but appstore + heroku with production
-  Parse.init application_id: "m7UOgUNoihu3JP3Zn9WcShFORaxkoanuTybOWDx8", api_key: "sHlZbb3wUgFoVXYPaCmRirwWx0LE2HNO3J4twNwK" # production
+  Parse.init application_id: "m7UOgUNoihu3JP3Zn9WcShFORaxkoanuTybOWDx8", api_key: "sHlZbb3wUgFoVXYPaCmRirwWx0LE2HNO3J4twNwK"
 end
