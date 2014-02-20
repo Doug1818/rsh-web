c48391dd24e74ecb9bff8b00fc6429e6da23a105
Doug Raicek <doug.raicek@gmail.com>
2014-02-20 16:00:59 -0500

    Move stripe keys to environment variable

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
   Stripe.api_key = "sk_live_Z7yfpHo0N0chGll4IwWBQ9S7"
-  STRIPE_PUBLIC_KEY = "pk_live_vMTyMR6p42Qtwix7r1W8OUny"
+  ENV['STRIPE_PUBLIC_KEY'] = "pk_live_vMTyMR6p42Qtwix7r1W8OUny"
 else
   Stripe.api_key = "sk_test_5VfCOBineyRvm6l07ZAUanJM"
-  STRIPE_PUBLIC_KEY = "pk_test_WHUKR5V6wB7I8VdYOOJIBB6v"
+  ENV['STRIPE_PUBLIC_KEY'] = "pk_test_WHUKR5V6wB7I8VdYOOJIBB6v"
 end
