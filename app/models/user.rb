b0420982039dc5e9275884d5612f8f2cbdf05e27
Doug Raicek <doug.raicek@gmail.com>
2014-02-25 11:48:46 -0500

    Update new user validations to run only on create

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :full_name, presence: true
+  validates :full_name, presence: true, :on => :create
 
   mount_uploader :avatar, AvatarUploader
 
