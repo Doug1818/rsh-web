2084e0b6b60fd62da6869b5eee358fffd295c303
Doug Raicek <doug.raicek@gmail.com>
2014-02-27 15:07:09 -0500

    Allow coaches to delete reminders

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -22,6 +22,16 @@ class RemindersController < ApplicationController
     end
   end
 
+  def destroy
+    @reminder = Reminder.find(params[:id])
+    @program = Program.find(@reminder.program_id)
+    @reminder.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(program_path(@program, active: 'advanced-settings'), notice: "Your reminder has been successfully deleted.") }
+    end
+  end
+
   def reminder_params
     params.require(:reminder).permit(:body, :frequency, :send_at, :send_on, :program_id, :monthly_recurrence, :weekly_recurrence, :daily_recurrence, :day_of_week, :start_date)
   end
