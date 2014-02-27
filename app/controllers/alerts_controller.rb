77839596a6c8ad14004a34ba6bb556902fae4216
Doug Raicek <doug.raicek@gmail.com>
2014-02-27 15:33:02 -0500

    Allow coaches to delete alerts

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -22,6 +22,16 @@ class AlertsController < ApplicationController
     end
   end
 
+  def destroy
+    @alert = Alert.find(params[:id])
+    @program = Program.find(@alert.program_id)
+    @alert.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(program_path(@program, active: 'advanced-settings'), notice: "Your alert has been successfully deleted.") }
+    end
+  end
+
   def alert_params
     params.require(:alert).permit(:action_type, :streak, :sequence, :program_id)
   end
